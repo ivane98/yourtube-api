a9cb66aa799861bd394a6282bcc61b3df423a4ae
ivane98 <i.vardoshvili22@gmail.com>
2023-03-30 17:52:18 +0400

    m

--- index.js
+++ index.js
@@ -22,11 +22,7 @@ const connect = () => {
       throw err;
     });
 };
-app.use(
-  cors({
-    origin: ["http://localhost:3001", "https://yourtube30.netlify.app"],
-  })
-);
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/users", userRoutes);
